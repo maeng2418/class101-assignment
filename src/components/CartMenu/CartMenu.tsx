710a0b354904a3852b8cf18510dc926dfc482a8f
maeng2418 <maeng2418@naver.com>
2020-11-16 11:43:01 +0900

    [feat] 장바구니 구현 #19 - 장바구니 추가 및 삭제 기능 구현 - 헤더 장바구니 아이콘에 갯수 표시

--- src/components/CartMenu/CartMenu.tsx
+++ src/components/CartMenu/CartMenu.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './styles.module.css';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useStateValue } from '../../context';
 
 export const CartMenu: React.FC = () => {
+  const [{ cart }, dispatch] = useStateValue();
   return (
     <div className={styles.cartMenu}>
       <AiOutlineShoppingCart />
+      <span className={styles.cartCount}>{cart?.length}</span>
     </div>
   );
 };
